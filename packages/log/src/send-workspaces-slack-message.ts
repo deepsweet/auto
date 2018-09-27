5022e576f8c6cdbfb9a7a65081be17432586d21d
alex.feinstein <alex.feinstein@klarna.com>
2018-09-27 11:12:00 +0200

    ✔️ log: limit max attachments

--- packages/log/src/send-workspaces-slack-message.ts
+++ packages/log/src/send-workspaces-slack-message.ts
@@ -1,6 +1,6 @@
 import request from 'request-promise-native'
 import { TPrefixes } from '@auto/utils/src'
-import { SLACK_HOOKS_URL } from './utils'
+import { SLACK_HOOKS_URL, MAX_ATTACHMENTS } from './utils'
 import { TSlackOptions, TWorkspacesLog } from './types'
 
 export const sendWorkspacesSlackMessage = async (logs: TWorkspacesLog[], prefixes: TPrefixes, options: TSlackOptions) => {
@@ -8,26 +8,35 @@ export const sendWorkspacesSlackMessage = async (logs: TWorkspacesLog[], prefixe
     throw new Error('Slack token is required')
   }
 
-  await request({
-    uri: `${SLACK_HOOKS_URL}${options.token}`,
-    method: 'POST',
-    json: {
-      channel: options.channel,
-      username: options.username,
-      link_names: '1',
-      icon_emoji: options.iconEmoji,
-      unfurl_media: false,
-      attachments: logs.map((bump) => ({
-        color: options.colors[bump.type],
-        fields: [
-          {
-            title: `${bump.name} v${bump.version}`,
-            value: bump.messages
-              .map((message) => `${prefixes.required[message.type].value} ${message.value}`)
-              .join('\n')
-          }
-        ]
-      }))
-    }
-  })
+  const allAttachments = logs.map((bump) => ({
+    color: options.colors[bump.type],
+    fields: [
+      {
+        title: `${bump.name} v${bump.version}`,
+        value: bump.messages
+          .map((message) => `${prefixes.required[message.type].value} ${message.value}`)
+          .join('\n')
+      }
+    ]
+  }))
+  const numIterations = Math.ceil(allAttachments.length / MAX_ATTACHMENTS)
+  const attachmentsInIteration = Math.ceil(allAttachments.length / numIterations)
+
+  for (let i = 0; i < numIterations; ++i) {
+    await request({
+      uri: `${SLACK_HOOKS_URL}${options.token}`,
+      method: 'POST',
+      json: {
+        channel: options.channel,
+        username: options.username,
+        link_names: '1',
+        icon_emoji: options.iconEmoji,
+        unfurl_media: false,
+        attachments: allAttachments.slice(
+          i * attachmentsInIteration,
+          Math.min((i + 1) * attachmentsInIteration, allAttachments.length)
+        )
+      }
+    })
+  }
 }
--- packages/log/test/send-workspaces-slack-message.ts
+++ packages/log/test/send-workspaces-slack-message.ts
@@ -162,3 +162,151 @@ test('sendWorkspacesSlackMessage: throws if there is no token', async (t) => {
 
   unmock('../src/send-workspaces-slack-message')
 })
+
+test('sendWorkspacesSlackMessage', async (t) => {
+  const spy = createSpy(() => Promise.resolve())
+
+  mock('../src/send-workspaces-slack-message', {
+    'request-promise-native': {
+      default: spy
+    },
+    './utils': {
+      SLACK_HOOKS_URL: 'https://hooks.slack.com/services/',
+      MAX_ATTACHMENTS: 3
+    }
+  })
+
+  const { sendWorkspacesSlackMessage } = await import('../src/send-workspaces-slack-message')
+
+  await sendWorkspacesSlackMessage(
+    [
+      {
+        name: 'a',
+        version: '0.1.2',
+        type: 'minor',
+        messages: [
+          {
+            type: 'minor',
+            value: 'minor'
+          }
+        ]
+      },
+      {
+        name: 'b',
+        version: '1.2.3',
+        type: 'minor',
+        messages: [
+          {
+            type: 'minor',
+            value: 'minor'
+          }
+        ]
+      },
+      {
+        name: 'c',
+        version: '1.2.3',
+        type: 'minor',
+        messages: [
+          {
+            type: 'minor',
+            value: 'minor'
+          }
+        ]
+      },
+      {
+        name: 'd',
+        version: '1.2.3',
+        type: 'minor',
+        messages: [
+          {
+            type: 'minor',
+            value: 'minor'
+          }
+        ]
+      },
+      {
+        name: 'e',
+        version: '1.2.3',
+        type: 'minor',
+        messages: [
+          {
+            type: 'minor',
+            value: 'minor'
+          }
+        ]
+      }
+    ],
+    prefixes,
+    slackOptions
+  )
+
+  t.deepEquals(
+    getSpyCalls(spy),
+    [
+      [{
+        uri: 'https://hooks.slack.com/services/token',
+        method: 'POST',
+        json: {
+          channel: 'channel',
+          username: 'username',
+          link_names: '1',
+          icon_emoji: 'emoji',
+          unfurl_media: false,
+          attachments: [
+            {
+              color: 'minor',
+              fields: [{
+                title: 'a v0.1.2',
+                value: `${prefixes.required.minor.value} minor`
+              }]
+            },
+            {
+              color: 'minor',
+              fields: [{
+                title: 'b v1.2.3',
+                value: `${prefixes.required.minor.value} minor`
+              }]
+            },
+            {
+              color: 'minor',
+              fields: [{
+                title: 'c v1.2.3',
+                value: `${prefixes.required.minor.value} minor`
+              }]
+            }
+          ]
+        }
+      }],
+      [{
+        uri: 'https://hooks.slack.com/services/token',
+        method: 'POST',
+        json: {
+          channel: 'channel',
+          username: 'username',
+          link_names: '1',
+          icon_emoji: 'emoji',
+          unfurl_media: false,
+          attachments: [
+            {
+              color: 'minor',
+              fields: [{
+                title: 'd v1.2.3',
+                value: `${prefixes.required.minor.value} minor`
+              }]
+            },
+            {
+              color: 'minor',
+              fields: [{
+                title: 'e v1.2.3',
+                value: `${prefixes.required.minor.value} minor`
+              }]
+            }
+          ]
+        }
+      }]
+    ],
+    'should make request'
+  )
+
+  unmock('../src/send-workspaces-slack-message')
+})
