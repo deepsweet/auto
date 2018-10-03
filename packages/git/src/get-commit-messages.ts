cc00c4fa09c3c6304dee305172dcbd6d610fb448
psxcode <psxcode@gmail.com>
2018-10-03 16:53:22 +0200

    ✔️ git: revert fix git log

--- packages/git/src/get-commit-messages.ts
+++ packages/git/src/get-commit-messages.ts
@@ -1,7 +1,7 @@
 import execa from 'execa'
 
 export const getCommitMessages = async (): Promise<string[]> => {
-  const { stdout } = await execa('git', ['log', '--pretty=format:%s', '--no-merges', '--first-parent'], {
+  const { stdout } = await execa('git', ['log', '--pretty=format:%s'], {
     stderr: null
   })
 
